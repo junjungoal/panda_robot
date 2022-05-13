32831352a1c2990b2d2b6803d1120d082c02e28a
junjungoal <junyamada107@gmail.com>
2022-05-13 21:42:54 +0100

    add new vel controller

--- panda_robot_control/include/panda_robot/cartesian_velocity_example_controller.h
+++ panda_robot_control/include/panda_robot/cartesian_velocity_example_controller.h
@@ -11,7 +11,11 @@
 #include <hardware_interface/robot_hw.h>
 #include <ros/node_handle.h>
 #include <ros/time.h>
-#include <geometry_msgs/Twist.h>
+
+#include <franka/rate_limiting.h>
+
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
 
 namespace panda_robot_controllers {
 
@@ -23,27 +27,29 @@ class CartesianVelocityExampleController : public controller_interface::MultiInt
   void update(const ros::Time&, const ros::Duration& period) override;
   void starting(const ros::Time&) override;
   void stopping(const ros::Time&) override;
-  void target_velocity_callback(const geometry_msgs::Twist::ConstPtr& msg);
+
+  void cartesian_velocity_callback(const geometry_msgs::Twist::ConstPtr& msg);
 
  private:
   franka_hw::FrankaVelocityCartesianInterface* velocity_cartesian_interface_;
-  franka_hw::FrankaStateInterface* state_interface;
-  std::string arm_id;
-
-  bool was_in_contact;
-  std::array<double, 6> ee_twist_cause_of_contact;
-  ros::Time time_since_last_target_velocity;
-  
   std::unique_ptr<franka_hw::FrankaCartesianVelocityHandle> velocity_cartesian_handle_;
-  ros::Duration elapsed_time_;
-  
-  geometry_msgs::Twist target_velocity;
-  geometry_msgs::Twist current_velocity;
-  
-  ros::Subscriber target_velocity_subscriber;
+  std::unique_ptr<franka_hw::FrankaStateHandle> state_handle_;
 
-  ros::Publisher current_velocity_publisher;
-  
+  std::array<double, 6> velocity_command;
+  std::array<double, 6> last_sent_velocity;
+  ros::Duration time_since_last_command;
+  ros::Subscriber velocity_command_subscriber;
+
+  // Parameters
+  double max_duration_between_commands;
+  double max_velocity_linear;
+  double max_acceleration_linear;
+  double max_jerk_linear;
+  double max_velocity_angular;
+  double max_acceleration_angular;
+  double max_jerk_angular;
+  bool stop_on_contact;
+
 };
 
-}  
+}  // namespace panda_robot_controllers
